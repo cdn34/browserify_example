93661c689bfc117915aa279f829bb0b1cb208bd1
cdn34 <carlos.nogueira04@gmail.com>
2016-05-09 23:18:57 +0100

    Added watcher to npm scripts

--- app/app.js
+++ app/app.js
@@ -5,9 +5,13 @@ var $ = require('jquery');
 
 // and then go ahead and use them as we wish:
 $('.element').on('click',function() {
-    if($('.element').hasClass('element-round'))
+    if($('.element').hasClass('element-round')){
         $('.element').removeClass( 'element-round');
-    else
+        $('#text').text('Square');
+    }else{
         $('.element').addClass('element-round');
+        $('#text').text('Circle');
+    }
+        
     
 });
